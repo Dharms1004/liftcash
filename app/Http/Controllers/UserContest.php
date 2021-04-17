bc55a3fb859bd49b31e0c667ee93c60199061d91
Dharms1004 <dharms1004@gmail.com>
2021-04-17 14:25:23 +0530

    watch-video-api-and-signup-bonus

--- app/Http/Controllers/UserContest.php
+++ app/Http/Controllers/UserContest.php
@@ -35,14 +35,9 @@ class UserContest extends Controller
         $getSpinLimit = $this->getSpinLimit($check_token->USER_ID); /** get user's spin count balance */
 
         $spinLimit = env('SPIN_LIMIT');
-
-        $openingClosingBalace = $this->getOpeningClosingBalace($check_token->USER_ID); /** get opening and closing balance */
         
-        $oldClosingBalance = $openingClosingBalace->CLOSING_TOT_BALANCE ?? 0;
-        $oldCurrentBalance = $openingClosingBalace->CURRENT_TOT_BALANCE ?? 0;
-        
-        $newCurrentBalance = $oldCurrentBalance + $coinsToBeCredit;
-        $newClosingBalance = $oldCurrentBalance;
+        $currentTotBalance = $userBalance->BALANCE;
+        $closingTotBalance = $currentTotBalance + $coinsToBeCredit;
 
         date_default_timezone_set('Asia/Kolkata');
 		$currentDate = date('Y-m-d H:i:s');
@@ -65,8 +60,8 @@ class UserContest extends Controller
                     "PAY_MODE" => "",
                     "INTERNAL_REFERENCE_NO" => $internalRefNo,
                     "PAYOUT_NUMBER" => "",
-                    "CURRENT_TOT_BALANCE" => $newCurrentBalance,
-                    "CLOSING_TOT_BALANCE" => $newClosingBalance,
+                    "CURRENT_TOT_BALANCE" => $currentTotBalance,
+                    "CLOSING_TOT_BALANCE" => $closingTotBalance,
                     "TRANSACTION_DATE" => $currentDate
                 ];
 
@@ -122,15 +117,10 @@ class UserContest extends Controller
         $userBalance = $this->getUserBalance($check_token->USER_ID); /** get user's current balance */
         $getSpinLimit = $this->getScratchCardLimit($check_token->USER_ID); /** get user's spin count balance */
 
-        $spinLimit = env('SPIN_LIMIT');
+        $spinLimit = env('SCRATCH_LIMIT');
 
-        $openingClosingBalace = $this->getOpeningClosingBalace($check_token->USER_ID); /** get opening and closing balance */
-        
-        $oldClosingBalance = $openingClosingBalace->CLOSING_TOT_BALANCE ?? 0;
-        $oldCurrentBalance = $openingClosingBalace->CURRENT_TOT_BALANCE ?? 0;
-        
-        $newCurrentBalance = $oldCurrentBalance + $coinsToBeCredit;
-        $newClosingBalance = $oldCurrentBalance;
+        $currentTotBalance = $userBalance->BALANCE;
+        $closingTotBalance = $currentTotBalance + $coinsToBeCredit;
 
         date_default_timezone_set('Asia/Kolkata');
 		$currentDate = date('Y-m-d H:i:s');
@@ -153,8 +143,8 @@ class UserContest extends Controller
                     "PAY_MODE" => "",
                     "INTERNAL_REFERENCE_NO" => $internalRefNo,
                     "PAYOUT_NUMBER" => "",
-                    "CURRENT_TOT_BALANCE" => $newCurrentBalance,
-                    "CLOSING_TOT_BALANCE" => $newClosingBalance,
+                    "CURRENT_TOT_BALANCE" => $currentTotBalance,
+                    "CLOSING_TOT_BALANCE" => $closingTotBalance,
                     "TRANSACTION_DATE" => $currentDate
                 ];
 
@@ -212,5 +202,75 @@ class UserContest extends Controller
             ->where(["USER_ID" => $userId, "TRANSACTION_TYPE_ID" => $spinTransactionTypeId, "TRANSACTION_STATUS_ID" => $spinTransactionStatusSuccess])
             ->whereBetween('TRANSACTION_DATE',[$startTime, $endTime])
             ->count();
+    }
+
+
+    /** get coins via watching adds */
+
+    public function watchVideo(Request $request)
+    {
+        $rules = [
+            'userId' => 'required|max:10',
+            'versionName' => 'required|max:100',
+            'versionCode' => 'required|max:100',
+            'api_token' => 'required|max:100',
+            'coinsToBeCredit' => 'required'
+
+        ];
+        $customMessages = [
+            'required' => 'Please fill required :attribute'
+        ];
+
+        $this->validate($request, $rules, $customMessages);
+        $token = $request->input('api_token');
+        $coinsToBeCredit = $request->input('coinsToBeCredit');
+        $check_token = User::where('API_TOKEN', $token)->select('USER_ID')->first();
+
+        $userBalance = $this->getUserBalance($check_token->USER_ID); /** get user's current balance */
+
+        $currentTotBalance = $userBalance->BALANCE;
+        $closingTotBalance = $currentTotBalance + $coinsToBeCredit;
+
+        date_default_timezone_set('Asia/Kolkata');
+		$currentDate = date('Y-m-d H:i:s');
+
+        $internalRefNo = "111" . $check_token->USER_ID;
+		$internalRefNo = $internalRefNo . mt_rand(100, 999);
+		$internalRefNo = $internalRefNo . $this->getDateTimeInMicroseconds();
+		$internalRefNo = $internalRefNo . mt_rand(100, 999);
+
+        try{
+            $transData = [
+                "USER_ID" => $check_token->USER_ID,
+                "BALANCE_TYPE_ID" => 1,
+                "TRANSACTION_STATUS_ID" => 1, /** for coins credited succesfully */
+                "TRANSACTION_TYPE_ID" => 5, /** for coins credited from Watching Ads */
+                "PAYOUT_COIN" => $coinsToBeCredit,
+                "PAYOUT_EMIAL" => "",
+                "PAY_MODE" => "",
+                "INTERNAL_REFERENCE_NO" => $internalRefNo,
+                "PAYOUT_NUMBER" => "",
+                "CURRENT_TOT_BALANCE" => $currentTotBalance,
+                "CLOSING_TOT_BALANCE" => $closingTotBalance,
+                "TRANSACTION_DATE" => $currentDate
+            ];
+
+            $userNewBalance = $userBalance->BALANCE + $coinsToBeCredit;
+
+            $this->creditOrDebitCoinsToUser($transData);
+            $this->updateUserBalance($userNewBalance, $check_token->USER_ID);
+
+            $res['status'] = '200';
+            $res['message'] = 'Success';
+            $res['type'] = 'Coin_added_success';
+            return response($res);
+
+        }catch(\Illuminate\Database\QueryException $e){
+            $res['status'] = false;
+            $res['message'] = $e;
+            $res['type'] = 'some_error_occured';
+            return response($res);
+        }
+
     }
 }
